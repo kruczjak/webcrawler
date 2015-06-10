4a328bda7b1e585eb68cc6f0763224bdc90648cf
kruczjak <kruczjak@gmail.com>
2015-06-11 00:41:47 +0200

    Futures part 2

--- src/main/scala/WebCrawler.scala
+++ src/main/scala/WebCrawler.scala
@@ -12,31 +12,29 @@ object WebCrawler extends App {
   import org.htmlcleaner.HtmlCleaner
   import java.net.URL
 
-  def operation(url: String, level: Int, maxLevel: Int): Future[(Array[TagNode], Int, Int)] = Future {
+  def operation(url: String, level: Int): Future[(Array[TagNode], Int)] = Future {
     val cleaner = new HtmlCleaner
     val rootNode = cleaner.clean(new URL(url))
-    (rootNode.getElementsByName("a", true), level, maxLevel)
+    (rootNode.getElementsByName("a", true), level)
   }
 
   val maxLevel = readInt()
   val url = "http://google.com"
-  val start = operation(url, 0, maxLevel)
+  val start = operation(url, maxLevel)
 
   start onComplete {
-    case Success((elements, level, maxLevel)) => {
-
-    }
+    case Success((elements, level)) =>
+      elements map { elem =>
+        val url = elem.getAttributeByName("href")
+        println(url.toString)
+        level match {
+          case 0 =>
+          case _ => operation(url.toString, level-1)
+        }
+      }
     case Failure(err) => println("Error: " + err.getMessage)
   }
 
-//  val cleaner = new HtmlCleaner
-//  val rootNode = cleaner.clean(new URL(url))
-//  val elements = rootNode.getElementsByName("a", true)
-//  elements map { elem =>
-//    val url = elem.getAttributeByName("href")
-//    println(url.toString)
-//  }
-
-  Await.result()
+  Thread.sleep(3000)
 
 }
