2216a235baf6e5b33c58c8d365d8a2b96594bee2
kruczjak <kruczjak@gmail.com>
2015-06-11 13:12:41 +0200

    Recursive works (but not quite well)

--- src/main/scala/WebCrawler.scala
+++ src/main/scala/WebCrawler.scala
@@ -8,33 +8,48 @@ import scala.util.{Failure, Success}
 object WebCrawler extends App {
   import scala.io.Source
   import scala.concurrent._
+  import ExecutionContext.Implicits.global
   import scala.io.StdIn.readInt
   import org.htmlcleaner.HtmlCleaner
   import java.net.URL
 
-  def operation(url: String, level: Int): Future[(Array[TagNode], Int)] = Future {
+  def operation(url: String, level: Int, list: List[String]): Future[(Array[TagNode], Int, List[String])] = Future {
+    list :+ url
     val cleaner = new HtmlCleaner
     val rootNode = cleaner.clean(new URL(url))
-    (rootNode.getElementsByName("a", true), level)
+    (rootNode.getElementsByName("a", true), level, list)
   }
 
-  val maxLevel = readInt()
-  val url = "http://google.com"
-  val start = operation(url, maxLevel)
+//  val maxLevel = readInt()
+  val maxLevel = 5
+  val url = "http://interia.pl"
+  val start = operation(url, maxLevel, List[String]())
+
+  def ok(elements: Array[TagNode], level:Int, list: List[String]): Unit = {
+    elements foreach { elem =>
+      val url = elem.getAttributeByName("href")
+      println(maxLevel - level + " " + url.toString)
+      level match {
+        case 0 =>
+        case _ =>
+          list.contains(url.toString) match {
+            case false =>
+              val f = operation(url.toString, level-1, list)
+              f onComplete {
+                case Success((a, b, l)) => ok(a, b, l)
+                case Failure(err) =>  println("Error: " + err.getMessage)
+              }
+            case true =>
+          }
+      }
+    }
+  }
 
   start onComplete {
-    case Success((elements, level)) =>
-      elements map { elem =>
-        val url = elem.getAttributeByName("href")
-        println(url.toString)
-        level match {
-          case 0 =>
-          case _ => operation(url.toString, level-1)
-        }
-      }
-    case Failure(err) => println("Error: " + err.getMessage)
+    case Success((a, b, l)) => ok(a, b, l)
+    case Failure(err) =>  println("Error: " + err.getMessage)
   }
 
-  Thread.sleep(3000)
+  Thread.sleep(990000)
 
 }
